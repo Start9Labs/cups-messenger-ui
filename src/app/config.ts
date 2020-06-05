f9a62a8343f396334a97bce3ff8f690da4c8efb2
Aaron Greenspan <afg419@gmail.com>
2020-06-05 00:06:48 -0600

    failed message styling

--- src/app/config.ts
+++ src/app/config.ts
@@ -46,7 +46,7 @@ export interface Config {
 
 export const config: Config = {
     cupsMessenger: {
-        type: CupsMessengerType.STANDARD_MOCK,
+        type: CupsMessengerType.ERROR_MOCK,
         url: '/api'
     },
     contactsDaemon: {
