62007ed199477e54fe3611c88ba502e0616ee979
Aiden McClelland <me@drbonez.dev>
2020-06-12 15:47:35 -0600

    auto deploy in build-send-demo

--- build-send-demo.sh
+++ build-send-demo.sh
@@ -6,6 +6,7 @@ rm -rf www
 npm run build-prod
 tar -zcvf cups${1}.tar.gz www
 scp cups${1}.tar.gz root@demo.start9labs.com:/root/cups
+ssh root@demo.start9labs.com "rm -rf /root/www && /bin/tar -zxvf /root/cups/cups${1}.tar.gz -C /root"
 sha1sum cups${1}.tar.gz
 #rm cups${1}.tar.gz
 #ssh pi@start9-aec79475.local "sudo /home/pi/cups/pack.sh"
